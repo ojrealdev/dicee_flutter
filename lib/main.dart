2e19bf303883966848d1a9876416bff4214d7706
ojrealdev <stephenojwang11@gmail.com>
2022-02-01 23:11:11 +0300

    added fonts and more

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
-}
+void main() => runApp(DicePage());
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Dicee by Ochieng',
+              style:
+                  TextStyle(color: Colors.blue, fontFamily: 'Supermercado One'),
+            ),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: Container(
+          child: Center(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: Image.network('images/dice1.png'),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: Image.asset('images/dice2.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
